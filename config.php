cc1f8d5098af0d2dc58aaec799fe71caa39becf8
Coach Mark <coachmarcbrewer@gmail.com>
2021-08-04 15:00:40 +0600

    first commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "wonder";
+
+// Site URL
+$site_url = "http://localhost:8080/wonder"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "3b5241aa-3029-4be9-89e9-357372e5f21f"; // Your purchase code, don't give it to anyone. 
+?>
