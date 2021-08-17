276b3af955dd0f0525778eb130aa050900ca8bf9
Mofakhkherul Islam <rojoni25@gmail.com>
2021-08-17 22:30:59 +0600

    Working with ads

--- assets/includes/custom_functions.php
+++ assets/includes/custom_functions.php
@@ -1,5 +1,52 @@
 <?php
 
+function Wo_GetAdId($type, $admin = true) {
+    global $sqlConnect;
+    $type      = Wo_Secure($type);
+    $query_one = "SELECT `id` FROM " . T_ADS . " WHERE `type` = '{$type}'";
+    if ($admin === false) {
+        $query_one .= " AND `active` = '1'";
+    }
+    $sql_query_one = mysqli_query($sqlConnect, $query_one);
+    if (mysqli_num_rows($sql_query_one)) {
+        $fetched_data  = mysqli_fetch_assoc($sql_query_one);
+        return $fetched_data['id'];
+    }
+    return false;
+        
+}
+function Wo_GetAdPoint($type, $admin = true) {
+    global $sqlConnect;
+    $type      = Wo_Secure($type);
+    $query_one = "SELECT `points` FROM " . T_ADS . " WHERE `type` = '{$type}'";
+    if ($admin === false) {
+        $query_one .= " AND `active` = '1'";
+    }
+    $sql_query_one = mysqli_query($sqlConnect, $query_one);
+    if (mysqli_num_rows($sql_query_one)) {
+        $fetched_data  = mysqli_fetch_assoc($sql_query_one);
+        return $fetched_data['points'];
+    }
+    return false;
+        
+}
+
+function Wo_GetAdTimer($type, $admin = true) {
+    global $sqlConnect;
+    $type      = Wo_Secure($type);
+    $query_one = "SELECT `timer` FROM " . T_ADS . " WHERE `type` = '{$type}'";
+    if ($admin === false) {
+        $query_one .= " AND `active` = '1'";
+    }
+    $sql_query_one = mysqli_query($sqlConnect, $query_one);
+    if (mysqli_num_rows($sql_query_one)) {
+        $fetched_data  = mysqli_fetch_assoc($sql_query_one);
+        return $fetched_data['timer'];
+    }
+    return false;
+        
+}
+
 function ro_userAdsAssignment($adid){    
     
     global $wo, $sqlConnect;
