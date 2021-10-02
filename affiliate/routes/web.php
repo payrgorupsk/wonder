f762ea1c7e8e524ae5aa428bac694932bedd1f31
Coach Mark <coachmarcbrewer@gmail.com>
2021-10-03 00:19:56 +0600

    affiliate form submit

--- affiliate/routes/web.php
+++ affiliate/routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/lol', [HomeController::class, 'index']);
+Route::get('/affiliate_form', [HomeController::class, 'form'])->name('affiliate_form');
+Route::post('/affiliate_form', [HomeController::class, 'form_save'])->name('affiliate_form_save');
